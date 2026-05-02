855c48ed77b2d3337c7c976195d09386b69c1e47
Wu28ri <hower@begonia.community>
2026-05-03 00:39:44 +1200

    expose Track as Q_GADGET for QML

--- src/Track.h
+++ src/Track.h
@@ -1,8 +1,19 @@
 #pragma once
 
+#include <QMetaType>
 #include <QString>
 
 struct Track {
+    Q_GADGET
+    Q_PROPERTY(QString path     MEMBER path)
+    Q_PROPERTY(QString title    MEMBER title)
+    Q_PROPERTY(QString artist   MEMBER artist)
+    Q_PROPERTY(QString album    MEMBER album)
+    Q_PROPERTY(int     duration MEMBER duration)
+    Q_PROPERTY(QString techInfo MEMBER techInfo)
+    Q_PROPERTY(int     trackNo  MEMBER trackNo)
+
+public:
     QString path;
     QString title;
     QString artist;
@@ -11,5 +22,7 @@ struct Track {
     QString techInfo;
     int trackNo = 0;
 
-    bool isValid() const { return !path.isEmpty(); }
+    Q_INVOKABLE bool isValid() const { return !path.isEmpty(); }
 };
+
+Q_DECLARE_METATYPE(Track)
